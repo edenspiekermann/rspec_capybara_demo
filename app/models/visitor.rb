dba1b4241a1ffd1d7a5fc1d8201ee4969e8d6f0a
Oliver Zeyen <o.zeyen@de.edenspiekermann.com>
2014-01-21 17:39:42 +0100

    removed mail chimp from visitor model

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,15 +5,8 @@ class Visitor < ActiveRecord::Base
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-    mailchimp = Gibbon::API.new
-    result = mailchimp.lists.subscribe({
-      :id => ENV['MAILCHIMP_LIST_ID'],
-      :email => {:email => self.email},
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
-    })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    
+    Rails.logger.info("Subscribed fake email")
   end
 
 end
